29c809048fa1f699cf2408186329fd7d04b249b4
TechDetective <vishwajeetsinhjadeja4@gmail.com>
2017-05-05 14:13:40 +0530

    Image and autoscroll

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -3,11 +3,13 @@ $(document).ready(function(){
   $('.slider8').bxSlider({
     mode: 'vertical',
     minSlides: 1,
+    auto: true,
     slideMargin: 10
   });
   $('#achieve').bxSlider({
     mode: 'vertical',
     minSlides: 1,
-    slideMargin: 10
+    slideMargin: 10,
+    auto: true
   });
 });
